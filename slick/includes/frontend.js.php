5db1bec81b687d45bdcac6cb8f8473f04b983528
Thomas McMahon <tmcmahon@nerdery.com>
2017-04-30 21:07:52 -0500

    Re-working the carousel init's so that the dots are right on multiple slide carousels.

--- slick/includes/frontend.js.php
+++ slick/includes/frontend.js.php
@@ -31,33 +31,44 @@ var $slickSlider_bb_slidesToScroll = <?php echo $settings->slidesToScroll; ?>;
 var $slickSlider_bb_oneSlide = <?php echo $settings->oneSlide; ?>;
 var $slickSlider_bb_verticalCarousel = <?php echo $settings->verticalCarousel; ?>;
 
-    $slickSlider_bb.slick({
-        autoplay: $slickSlider_bb_autoplay,
-        autoplaySpeed: $slickSlider_bb_autoplaySpeed,
-        arrows: $slickSlider_bb_arrows,
-        dots: $slickSlider_bb_dots,
-        pauseOnHover: $slickSlider_bb_pauseOnHover,
-        pauseOnDotsHover: $slickSlider_bb_pauseOnDotsHover,
-        vertical: $slickSlider_bb_verticalCarousel,
-        infinite: $slickSlider_bb_infinite,
-        nextArrow: '<button class="fa fa-angle-right slick-arrow slick-next" aria-hidden="true"><span class="tmc_isVisibilyHidden">Next</span></button>',
-        prevArrow: '<button class="fa fa-angle-left slick-arrow slick-prev" aria-hidden="true"><span class="tmc_isVisibilyHidden">Previous</span></button>',
-    });
-
+    /* we have to setup the slider based on the number of slides we're showing and scrolling */
     if ($slickSlider_bb_oneSlide == true) {
-        $slickSlider_bb.slick("slickSetOption", "adaptiveHeight", $slickSlider_bb_adaptiveHeight, false);
+        $slickSlider_bb.slick({
+            autoplay: $slickSlider_bb_autoplay,
+            autoplaySpeed: $slickSlider_bb_autoplaySpeed,
+            arrows: $slickSlider_bb_arrows,
+            dots: $slickSlider_bb_dots,
+            pauseOnHover: $slickSlider_bb_pauseOnHover,
+            pauseOnDotsHover: $slickSlider_bb_pauseOnDotsHover,
+            vertical: $slickSlider_bb_verticalCarousel,
+            infinite: $slickSlider_bb_infinite,
+            adaptiveHeight: $slickSlider_bb_adaptiveHeight,
+            nextArrow: '<button class="fa fa-angle-right slick-arrow slick-next" aria-hidden="true"><span class="tmc_isVisibilyHidden">Next</span></button>',
+            prevArrow: '<button class="fa fa-angle-left slick-arrow slick-prev" aria-hidden="true"><span class="tmc_isVisibilyHidden">Previous</span></button>',
+        });
+    } else {
+        $slickSlider_bb.slick({
+            autoplay: $slickSlider_bb_autoplay,
+            autoplaySpeed: $slickSlider_bb_autoplaySpeed,
+            arrows: $slickSlider_bb_arrows,
+            dots: $slickSlider_bb_dots,
+            pauseOnHover: $slickSlider_bb_pauseOnHover,
+            pauseOnDotsHover: $slickSlider_bb_pauseOnDotsHover,
+            vertical: $slickSlider_bb_verticalCarousel,
+            infinite: $slickSlider_bb_infinite,
+            slidesToShow: $slickSlider_bb_slidesToShow,
+            slidesToScroll: $slickSlider_bb_slidesToScroll,
+            variableWidth: $slickSlider_bb_variableWidth,
+            centerMode: $slickSlider_bb_centerMode,
+            nextArrow: '<button class="fa fa-angle-right slick-arrow slick-next" aria-hidden="true"><span class="tmc_isVisibilyHidden">Next</span></button>',
+            prevArrow: '<button class="fa fa-angle-left slick-arrow slick-prev" aria-hidden="true"><span class="tmc_isVisibilyHidden">Previous</span></button>',
+        });
     }
+
 
     /* only use fade setting when using a horizontal carousel */
     if ($slickSlider_bb_verticalCarousel == false) {
         $slickSlider_bb.slick("slickSetOption", "fade", $slickSlider_bb_fade, false);
-    }
-
-    if ($slickSlider_bb_oneSlide == false) {
-        $slickSlider_bb.slick("slickSetOption", "slidesToShow", $slickSlider_bb_slidesToShow, false);
-        $slickSlider_bb.slick("slickSetOption", "slidesToScroll", $slickSlider_bb_slidesToScroll, false);
-        $slickSlider_bb.slick("slickSetOption", "variableWidth", $slickSlider_bb_variableWidth, false);
-        $slickSlider_bb.slick("slickSetOption", "centerMode", $slickSlider_bb_centerMode, false);
     }
 
     if ($slickSlider_bb_autoplay === false) {
