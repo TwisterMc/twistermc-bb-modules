d40d15b99065b9190357c6d421622eef6f5c8d8a
Thomas McMahon <tmcmahon@nerdery.com>
2018-04-17 20:17:15 -0500

    Making a few updates to adhere better to WordPress standards

--- slick/includes/frontend.php
+++ slick/includes/frontend.php
@@ -13,35 +13,35 @@
 ?>
 
 <div class="slickModule_bb">
-    <div class="slickWrapper_bb">
-        <?php
+	<div class="slickWrapper_bb">
+		<?php
 
-        if ($settings->photoVideo === 'photo') {
-	        $arr = $settings->multiple_photos_field;
-	        foreach ( $arr as &$value ) {
-		        echo '<li>';
-		        echo wp_get_attachment_image( $value, 'large', "", array( "class" => "img-responsive" ) );
+		if ( $settings->photoVideo === 'photo' ) {
+			$arr = $settings->multiple_photos_field;
+			foreach ( $arr as &$value ) {
+				echo '<li>';
+				echo wp_get_attachment_image( $value, 'large', '', array( 'class' => 'img-responsive' ) );
 
-		        if ( $settings->showCaptions == 'true' ) {
-			        $imageInfo = get_post( $value );
-			        echo '<div class="slickPhotoCaption">' . $imageInfo->post_excerpt . '</div>';
-		        }
+				if ( $settings->showCaptions === 'true' ) {
+					$imageInfo = get_post( $value );
+					echo '<div class="slickPhotoCaption">' . $imageInfo->post_excerpt . '</div>';
+				}
 
-		        echo '</li>';
-	        }
-        } else {
-	        $arr = $settings->multiple_video_field;
-	        foreach ( $arr as &$value ) {
-		        echo '<li>';
-		        echo '<div class="videoWrapper">';
-		        $embed_code = wp_oembed_get($value);
-		        echo $embed_code;
-		        echo '</div>';
-		        echo '</li>';
-	        }
-        }
-        ?>
-    </div>
-    <button class="fa fa-pause-circle js-slickModule_bb_Pause slickModule_bb_Pause" aria-hidden="true"><span class="tmc_isVisibilyHidden">Pause</span></button>
+				echo '</li>';
+			}
+		} else {
+			$arr = $settings->multiple_video_field;
+			foreach ( $arr as &$value ) {
+				echo '<li>';
+				echo '<div class="videoWrapper">';
+				$embed_code = wp_oembed_get( $value );
+				echo $embed_code;
+				echo '</div>';
+				echo '</li>';
+			}
+		}
+		?>
+	</div>
+	<button class="fa fa-pause-circle js-slickModule_bb_Pause slickModule_bb_Pause" aria-hidden="true"><span class="tmc_isVisibilyHidden">Pause</span></button>
 </div>
 
