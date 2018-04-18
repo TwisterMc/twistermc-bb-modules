d56572e44678d8dab99c920752bbf114eea8eb58
Thomas McMahon <tmcmahon@nerdery.com>
2018-04-17 21:03:01 -0500

    Updating to better align with WordPress standards.

--- fl-twistermc-modules.php
+++ fl-twistermc-modules.php
@@ -15,11 +15,11 @@ define( 'TMC_BB_URL', plugins_url( '/', __FILE__ ) );
  */
 function fl_load_module_bbtwistermc() {
 	if ( class_exists( 'FLBuilder' ) ) {
-	    require_once 'slick/slick.php';
-	    require_once 'fullImage/fullImage.php';
+		require_once 'slick/slick.php';
+		require_once 'fullImage/fullImage.php';
 	}
 }
-add_action( 'init', 'fl_load_module_bbtwistermc');
+add_action( 'init', 'fl_load_module_bbtwistermc' );
 
 /* ---------------------------------------------------------------------
 Enqueue Vimeo Helper Script
@@ -33,26 +33,26 @@ add_action( 'wp_enqueue_scripts', 'bbtwistermc_video_field_helper' );
 /**
  * Adds video attributes query strings to embedded YouTube videos
  */
-add_filter('oembed_result','oembed_result', 10, 3);
+add_filter( 'oembed_result', 'oembed_result', 10, 3 );
 
-function oembed_result($html, $url, $args) {
-	return str_replace("?feature=oembed", "?feature=oembed&loop=1&controls=0&showinfo=0&rel=0&enablejsapi=1", $html);
+function oembed_result( $html, $url, $args ) {
+	return str_replace( '?feature=oembed', '?feature=oembed&loop=1&controls=0&showinfo=0&rel=0&enablejsapi=1', $html );
 }
 
 /**
  * Adds video attributes query strings to embedded Vimeo videos
  */
-add_filter('oembed_fetch_url','add_video_args',10,3);
-function add_video_args($provider, $url, $args) {
-	if ( strpos($provider, '//vimeo.com/') !== false ) {
-		$args = array(
-			'title' => 0,
-			'byline' => 0,
-			'portrait' => 0,
-			'badge' => 0,
+add_filter( 'oembed_fetch_url', 'add_video_args', 10, 3 );
+function add_video_args( $provider, $url, $args ) {
+	if ( strpos( $provider, '//vimeo.com/' ) !== false ) {
+		$args     = array(
+			'title'       => 0,
+			'byline'      => 0,
+			'portrait'    => 0,
+			'badge'       => 0,
 			//'autoplay' => 1,
-			'loop' => 1,
-			'transparent' => 0
+			'loop'        => 1,
+			'transparent' => 0,
 		);
 		$provider = add_query_arg( $args, $provider );
 	}
